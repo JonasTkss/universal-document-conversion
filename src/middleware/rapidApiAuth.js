e2fe08433ae26a700805fd760482a5dd46464501
JonasToks <jonas.toks@bluelions.ee>
2025-01-28 09:44:03 +0200

    changes

--- src/middleware/rapidApiAuth.js
+++ src/middleware/rapidApiAuth.js
@@ -2,14 +2,9 @@ const ApiError = require("../utils/apiError");
 
 const rapidApiAuth = (req, res, next) => {
   const rapidApiKey = req.headers["x-rapidapi-key"];
-  const rapidApiProxy = req.headers["x-rapidapi-proxy-secret"];
-  const rapidApiHost = req.headers["x-rapidapi-host"];
 
-  if (!rapidApiKey || !rapidApiProxy || !rapidApiHost) {
-    throw new ApiError(
-      401,
-      "Unauthorized. This API is only accessible through RapidAPI."
-    );
+  if (!rapidApiKey) {
+    throw new ApiError(401, "API key is required");
   }
 
   const subscriptionTier =
